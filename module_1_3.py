17a3a467947a5947c7b2162ed42046b4b72c5aff
karvsd <karsten71@yandex.ru>
2024-08-18 21:44:24 +0200

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,6 @@ name = 'Vadim'
 print('Name:', name)
 age = 52
 print('Age:', age)
-new_age = age + 1
-new_age = print('New Age:', new_age)
+age = print('New Age:', age + 1)
 is_student = True
 print('is_student:', is_student)
