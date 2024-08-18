5665105b7f4fe4d94127e209512d6070ab2d83fe
karvsd <karsten71@yandex.ru>
2024-08-18 13:43:50 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+number_of_homework = '12'
+number_of_hours = '1.5'
+course_name = 'Python'
+time_for_on_task = float('1.5') / int('12')
+print('Курс:', course_name, ', всего задач:', number_of_homework, ', затрачено времени:', number_of_hours, ', среднее время выполнения: ', time_for_on_task)
