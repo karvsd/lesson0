a87800aaab14b813cc3a6431ca46eeb5f554e5c6
karvsd <karsten71@yandex.ru>
2024-09-03 19:10:36 +0200

    Add files via upload

--- module_3_1.py
+++ module_3_1.py
@@ -7,10 +7,11 @@ def string_info(string):
     return(len(string), string.upper(),string.lower())
 def is_contains(string, list_to_search):
     count_calls()
-    if string in list_to_search:
-        return(True)
-    else:
-        return(False)
+    string = string.lower()
+    list_to_search = [item.lower() for item in list_to_search]
+    return string in list_to_search
+
+
 print(string_info('Capybara'))
 print(string_info('Armagedon'))
 print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
