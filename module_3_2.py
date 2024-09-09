cbf8e7cab88c2ddb8babb94bc6305c51e66780da
karvsd <karsten71@yandex.ru>
2024-09-09 10:49:56 +0200

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,14 +1,14 @@
 def send_email(message, recipient, sender = 'university.help@gmail.com'):
-    # if (('@' not in (recipient or sender)) or (recipient.endswith(".com" or ".ru" or ".net") and
-    # sender.endswith(".com" or ".ru" or ".net"))):
-    if ("@" not in (recipient or sender) or ("@" or (".com" or ".ru" or ".net"))) not in (recipient or sender):
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
-    elif recipient == sender:
-        print('Нельзя отправить письмо самому себе!')
+    if recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
     elif sender == "university.help@gmail.com":
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
-    elif sender != "university.help@gmail.com":
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+        print("Письмо успешно отправлено с адреса", sender, "на адрес", recipient)
+    elif ('@' not in recipient or '@' not in sender) or not (recipient.endswith((".com", ".ru", ".net")) and
+        sender.endswith((".com", ".ru", ".net"))):
+        print("Невозможно отправить письмо с адреса ", sender, "на адрес", recipient)
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+
 
 
 send_email('Это сообщение для проверки связи','vasyok1337@gmail.com', sender = 'university.help@gmail.com')
