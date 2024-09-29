0128a087b65d933c8af2ab625964fe60ae045171
karvsd <karsten71@yandex.ru>
2024-09-29 17:05:50 +0200

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -9,76 +9,66 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-
     def __eq__(self, other):
         if isinstance(other, House):
-            return len(self) == len(other)
+            return self.number_of_floors == other.number_of_floors
         elif isinstance(other, int):
-            return len(self) == other
-        else:
-            return False
-
-
-    def __ne__(self, other):
-        if isinstance(other, House) or isinstance(other, int):
-            return not self == other
-        else:
-            return False
+            return self.number_of_floors == other
 
     def __lt__(self, other):
         if isinstance(other, House):
-            return len(self) < len(other)
+            return self.number_of_floors < other.number_of_floors
         elif isinstance(other, int):
-            return len(self) < other
-        else:
-            return False
+            return self.number_of_floors < other
 
-    def __ge__(self, other):
-        if isinstance(other, House) or isinstance(other, int):
-            return not self < other
-        else:
-            return False
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
 
     def __gt__(self, other):
         if isinstance(other, House):
-            return len(self) > len(other)
+            return self.number_of_floors > other.number_of_floors
         elif isinstance(other, int):
-            return len(self) > other
-        else:
-            return False
+            return self.number_of_floors > other
 
-    def __le__(self, other):
-        if isinstance(other, House) or isinstance(other, int):
-            return not self > other
-        else:
-            return False
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors >= other
 
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
 
     def __add__(self, value):
-        if isinstance(value, int):
-            return House(self.name, self.number_of_floors + value)
-        else:
-            print('Второе слагаемое должно быть целым числом')
-
+        if isinstance(value, House):
+            self.number_of_floors += value
+            return self
+        elif isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
     def __radd__(self, value):
-        if isinstance(value, int):
-            return self + value
-        else:
-            print('Число должно быть целым')
+        return self.__add__(value)
 
     def __iadd__(self, value):
-        if isinstance(value, int):
-            return self + value
-        else:
-            print('Число должно быть целым')
+        return self.__add__(value)
 
     def go_to(self, new_floor):
-        if int(new_floor) < 1 or int(new_floor) > int(self.number_of_floors):
+        self.new_floor = new_floor
+        if self.new_floor > self.number_of_floors or self.new_floor < 1:
             print('Такого этажа не существует')
         else:
-            for i in range(1, int(new_floor) + 1):
-                print(i)
+            for i in range(1, self.new_floor + 1):
+                if i == self.new_floor:
+                    print(f'Этаж номер: {i}, Вы приехали')
+                else:
+                    print(f'Этаж номер: {i}')
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -87,20 +77,20 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
+print(h1 == h2)  # __eq__
 
-h1 = h1 + 10 # __add__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
 
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
