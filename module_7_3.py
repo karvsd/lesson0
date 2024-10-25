bf428ae37ba1112aaa7f70db14e2bd7f21e3a22e
karvsd <karsten71@yandex.ru>
2024-10-25 22:45:30 +0200

    Add files via upload

--- module_7_3.py
+++ module_7_3.py
@@ -26,6 +26,6 @@ class WordsFinder:
 
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words())  # Все слова
-print(finder2.find('TEXT'))  # 3 слово по счёту
-print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+print(finder2.get_all_words())   # Все слова
+print(finder2.find('TEXT'))      # 3 слово по счёту
+print(finder2.count('teXT'))     # 4 слова teXT в тексте всего
